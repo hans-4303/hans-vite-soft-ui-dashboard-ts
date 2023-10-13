1518553deebb6cb6f728c12806fa408a31b71255
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-10-13 13:25:05 +0900

    패키지 읽기가 이상하지만 작동 중

---
+++ src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "/",
+    redirect: "/dashboard",
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
+  linkActiveClass: "active",
+});
+
+export default router;
