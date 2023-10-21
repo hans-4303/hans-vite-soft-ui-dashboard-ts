fb3c6cf84171b89048b47f2ed9dbf9f1b32fcfc5
definitiomea <103019360+definitiomea@users.noreply.github.com>
2023-10-21 14:11:47 +0900

    경로 수정

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
-import router from "./router";
+import App from "@/App.vue";
+import store from "@/store";
+import router from "@/router";
 
-import "./assets/css/nucleo-icons.css";
-import "./assets/css/nucleo-svg.css";
+import "@/assets/css/nucleo-icons.css";
+import "@/assets/css/nucleo-svg.css";
 
-import SoftUIDashboard from "./soft-ui-dashboard";
+import SoftUIDashboard from "@/soft-ui-dashboard";
 
 createApp(App).use(store).use(router).use(SoftUIDashboard).mount("#app");
